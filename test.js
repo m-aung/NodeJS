f483ee09b83e89fc4040491669fd8684f35a6c27
Myo Aung <myothuwinaung1991@gmail.com>
2021-05-27 23:38:27 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+/*
+// using readline
+const readline = require ('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+readline.question('Who are you? ', name => {
+    console.log(`Hey there ${name}`);
+    readline.close();
+});
+*/
