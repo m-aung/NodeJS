3840d2d428eda59e7b2eae561ffc41ddf5e4cdf8
Myo Aung <myothuwinaung1991@gmail.com>
2021-05-27 23:39:28 -0400

    Update test.js

--- test.js
+++ test.js
@@ -35,3 +35,60 @@ readline.question('Who are you? ', name => {
 //     detail: 'This is from emitWarning!', // detail <string> // print out in : 'This is from emitWarning'
 //     ctor: 'for stack trace' // ctor <Function>
 // });
+// Error Object
+// const errorObj = new Error('Some Error Object!');
+// errorObj.name = 'ERROR';
+// errorObj.code = '4291'; // 
+// process.emitWarning(errorObj);
+
+// emit only warning once
+// const emitMyWarning = () => {
+//     if(!this.warned){
+//         this.warned = true;
+//         // process.emitWarning object order --> type<string> --> code<string> --> ctor<Function> --> detail<string>
+//         process.emitWarning('Only warn once!','myWarning', '2920', );
+//     }
+// }
+// //first time
+// emitMyWarning(); // emit
+// // second time
+// emitMyWarning();
+
+//event: maxListeners
+// process.setMaxListeners(1);
+// process.on('foo', ()=>{});
+// process.on('foo', ()=>{});
+
+// process.stdin.resume();
+// const handle = (events) => {
+//     console.log(`${events}`)
+// }
+// process.on('SIGINT', handle);
+// process.on('SIGTERM', handle);
+
+// process.env
+// can delete or assign environment but everything saved in env is set to string
+
+// process.execPath
+
+// process.exit([code])
+// instead of using process.exit() use process.exitCode to wait for the WORKER process to end
+
+// process.hrtime ...to be continued
+
+//kill node process
+// process.kill(process.pid,signal_name) // process.on(signal_name, ()=>{})
+
+// memoryUsage
+// console.log(process.memoryUsage())
+/*
+example output:
+{
+    rss: 18530304, // rss <integer> --> Resident Set Size: total space occupied by main memory (C++ & JS:objecs and code)
+    heapTotal: 4014080, //heapTotal <integer> --> V8 memory usage
+    heapUsed: 2240344, // heapUsed <integer>
+    external: 767191, // external <integer> --> memory used by C++ objects bound to JS manage by V8
+    arrayBuffers: 9382 // arrayBuffers <integer> --> memory for all ArrayBuffer and SharedArrayBuffers
+}
+*/
+// can call each individual entities of memoryUsage objects
